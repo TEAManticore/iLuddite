463365a8a8c29fb498f889f1a88aa6af71425b51
Jason Barnett <jason@thebarnetts.ws>
2017-02-12 16:55:19 -0600

    solves axios route issues

--- app/axios.js
+++ app/axios.js
@@ -2,7 +2,7 @@ const axios = require('axios');
 
 // if in production set the url to localhost, otherwise production url
 let baseUrl;
-if (process.env.NODE_ENV === "production") {
+if (window.location.href.includes('herokuapp')) {
   baseUrl = 'https://i-luddite.herokuapp.com'
 } else {
   baseUrl = 'http://localhost:3000'
