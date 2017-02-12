daebca44136bfb7e98d9438a6c57eed5e2588e29
Jason Barnett <jason@thebarnetts.ws>
2017-02-12 16:49:40 -0600

    fixes axios base url

--- app/axios.js
+++ app/axios.js
@@ -1,11 +1,19 @@
 const axios = require('axios');
 
+// if in production set the url to localhost, otherwise production url
+let baseUrl;
+if (process.env.NODE_ENV === "production") {
+  baseUrl = 'https://i-luddite.herokuapp.com'
+} else {
+  baseUrl = 'http://localhost:3000'
+}
+
 // creates an instance of axios to use throughout the app
 // mostly just used so that in requests a relative path can be
 // typed instead of the full path
 // like get(/users/12345) instead of get(http://localhost:3000/users/12345)
 const instance = axios.create({
-  baseURL: process.env.HOST
+  baseURL: baseUrl
 });
 
 module.exports = instance;
